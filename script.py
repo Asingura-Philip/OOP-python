4fec225a57bc08eaec3830cd24cf6ce5742bf41f
Asingura Philip <asinguraphilip@gmail.com>
2024-09-06 23:23:21 +0300

    input ,print py

---
+++ script.py
@@ -0,0 +1,15 @@
+x = 4
+print(x)
+
+name = "Jackson"
+print(name)
+
+
+print("Hello, what is your name?")
+fname = input("enter firstname: ")
+print("nice to meet you " + fname)
+print("how old are you")
+age = input("enter age: ")
+
+print(fname + " is " + age + " years old")
+
