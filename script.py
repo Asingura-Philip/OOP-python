ade77a1318ebe6bf9be63ea543e5d8cd030f62a8
Asingura Philip <asinguraphilip@gmail.com>
2024-09-10 21:24:05 +0300

    conditions py

--- script.py
+++ script.py
@@ -1,18 +1,32 @@
 x = 4
-print(x)
+# print(x)
 
 name = "Jackson"
-print(name)
+# print(name)
 
 
-print("Hello, what is your name?")
-fname = input("enter firstname: ")
-print("nice to meet you " + fname)
-print("how old are you")
-age = input("enter age: ")
+# print("Hello, what is your name?")
+# fname = input("enter firstname: ")
+# print("nice to meet you " + fname)
+# print("how old are you")
+# age = input("enter age: ")
 
-print(fname + " is " + age + " years old")
+# print(fname + " is " + age + " years old")
+#  f strings
+# print(f"you are not so old {fname}")
 
-nums = [3,5,7]
+nums = [3, 5, 7]
 
-print(nums)
+# print(nums)
+
+# if statements
+
+age = int(input("Enter age: "))
+
+if age < 0:
+    print("not a valid age")
+elif age > 0:
+    print("valid age")
+else:
+    print("age is 0")
+
