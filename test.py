41601b48f5a168b62e4c334ac717bee699930fe6
Asingura Philip <asinguraphilip@gmail.com>
2024-09-21 20:36:42 +0300

    subtract

--- test.py
+++ test.py
@@ -8,10 +8,20 @@ def add(a,b):
 print('welcome to the calculator')
 print('select options')
 print('1.add\n 2.subtract')
-print('enter first number')
-number1 = int(input())
-print('enter second number')
-number2 = int(input())
+option = int(input())
+if option == 1:
+    print('enter first number')
+    number1 = int(input())
+    print('enter second number')
+    number2 = int(input())
+    sum = number1 + number2
+    print(f"the sum is {sum}")
 
-sum = number1 + number2
-print(f"the sum is {sum}")
+elif option == 2:
+    print('enter first number')
+    number1 = int(input())
+    print('enter second number')
+    number2 = int(input())
+    difference = number1 - number2
+    print(f"the difference is {difference}")
+
