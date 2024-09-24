c9dd78d60dcee5e0679805f2569489f808cdc7ae
Asingura Philip <asinguraphilip@gmail.com>
2024-09-24 21:02:19 +0300

    quotient

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ def add(a,b):
 # calc
 print('welcome to the calculator')
 print('select options')
-print('1.add\n 2.subtract\n 3.multiply')
+print('1.add\n 2.subtract\n 3.multiply \n 4.divide')
 option = int(input())
 if option == 1:
     print('enter first number')
@@ -32,3 +32,11 @@ elif option == 3:
     number2 = int(input())
     product = number1 * number2
     print(f"the product is {product}")
+
+elif option == 4:
+    print('enter first number')
+    number1 = int(input())
+    print('enter second number')
+    number2 = int(input())
+    quotient = number1 / number2
+    print(f"the quotient is {quotient}")
