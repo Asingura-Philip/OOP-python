82fb790347d76bfefeb004c78a0be31df64f2a42
Asingura Philip <asinguraphilip@gmail.com>
2024-10-01 21:57:49 +0300

    octa

--- script.py
+++ script.py
@@ -1,45 +1,48 @@
-# def my_generator():
-#     yield 1
-#     yield 2
-#     yield 3
+# # def my_generator():
+# #     yield 1
+# #     yield 2
+# #     yield 3
 
-# for value in my_generator():
-#     print(value)
+# # for value in my_generator():
+# #     print(value)
 
-# def my_function(*args):
-#     for arg in args:
-#     print(arg)
+# # def my_function(*args):
+# #     for arg in args:
+# #     print(arg)
 
-# my_function(1,2,3)
+# # my_function(1,2,3)
 
-x = 10
-def my_function():
-    global x
-    x = 20
-my_function()
-# print(x)
+# x = 10
+# def my_function():
+#     global x
+#     x = 20
+# my_function()
+# # print(x)
 
-# lambda
+# # lambda
 
-add = lambda x, y: x + y
-# print(add(2, 3))  # Output: 5
-
-
-numbers = [1, 2, 3, 4, 5, 6]
-evens = list(filter(lambda x: x % 2 == 0, numbers))
-# print(evens)  # Output: [2, 4, 6]
-
-tuples = [(1, 'one'), (2, 'two'), (3, 'three')]
-sorted_tuples = sorted(tuples, key=lambda x: x[1])
-# print(sorted_tuples)  # Output: [(1, 'one'), (3, 'three'), (2, 'two')]
+# add = lambda x, y: x + y
+# # print(add(2, 3))  # Output: 5
 
 
-class Dog:
-    def __init__(self,age, breed):
-        self.age = age
-        self.breed = breed
+# numbers = [1, 2, 3, 4, 5, 6]
+# evens = list(filter(lambda x: x % 2 == 0, numbers))
+# # print(evens)  # Output: [2, 4, 6]
+
+# tuples = [(1, 'one'), (2, 'two'), (3, 'three')]
+# sorted_tuples = sorted(tuples, key=lambda x: x[1])
+# # print(sorted_tuples)  # Output: [(1, 'one'), (3, 'three'), (2, 'two')]
+
+
+# class Dog:
+#     def __init__(self,age, breed):
+#         self.age = age
+#         self.breed = breed
     
-dog1 = Dog(14,"lab")
+# dog1 = Dog(14,"lab")
 
-print(dog1.age)
+# # print(dog1.age)
+
+gy = 9
+print(f"{gy:05d}")
     
