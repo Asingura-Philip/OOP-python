49f78d3e5e42ba63d1220f0eb37a58f134d4554c
Asingura Philip <asinguraphilip@gmail.com>
2024-10-05 22:44:06 +0300

    obj

--- assignment1.py
+++ assignment1.py
@@ -7,3 +7,8 @@ class Car:
         self.mileage = mileage
     def display_info(self):
         print(f"this is a {self.year} {self.brand} {self.model} with a mileage of {self.mileage}")
+
+
+car1 = Car("toyota","d3",2008,15000)
+car2 = Car("mercedes","yd3",2008,5230)
+car3 = Car("ford","CT3",2008,50000)
