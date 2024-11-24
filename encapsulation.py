aa89b44d38654780fd29e6e074c5e697d84c357e
Asingura Philip <asinguraphilip@gmail.com>
2024-11-24 22:04:36 +0300

    encapsulation oop

--- encapsulation.py
+++ encapsulation.py
@@ -5,9 +5,27 @@ class Vehicle:
         self.mileage = mileage
         self.__remaining_distance = self.mileage - self.traveled 
 
+    def drive(self):
+        print(f'the {self.model} vehicle is moving')
+
+class Taxi(Vehicle):
+    def __init__(self,model,traveled,speed,mileage):
+        super().__init__(model,traveled,mileage)
+        self.speed = speed
+
+    def drive(self):
+        super().drive()
+    # def drive(self):
+    #     print(f'the taxi moves at {self.speed} km per hr')
+
+
 car = Vehicle("benz",80,5000)
-# print(car.traveled)
-car.__remaining_distance = 50
-print(car.__remaining_distance)
+taxi1 = Taxi("noah",300,500,4000)
+car.drive()
+taxi1.drive()
+
+# # print(car.traveled)
+# car.__remaining_distance = 50
+# print(car.__remaining_distance)
 
     
